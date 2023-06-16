26586f0484f1d1d1fcf46a60a613968bdd5f7a52
Anastassy Cap <anacap100@gmail.com>
2023-06-15 21:39:19 -0400

    added better comment

--- print_string.asm
+++ print_string.asm
@@ -10,7 +10,8 @@ cmp al, 0 ; check if it is the null terminating character
 je end ; return if we are at the last value of the string
 
 int 0x10 ; print the char
-inc bx ; increment the address by a byte to point to the next character
+inc bx ; increment the address by a byte to point to the next character 
+       ; (we can also use add bx, 0x1 to go to the next address since each address accesses a byte of data)
 jmp print
 
 end:
